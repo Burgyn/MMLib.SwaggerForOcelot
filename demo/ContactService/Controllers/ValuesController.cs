c35ecf7464a4ff788f1288b883a01051f06fecfc
Miňo Martiniak <mino.martiniak@gmail.com>
2018-11-26 20:24:24 +0100

    Creating demo applications

---
+++ demo/ContactService/Controllers/ValuesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContactService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        /// <summary>
+        /// Get all values from project service.
+        /// </summary>
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+            => Ok(Enumerable.Range(0, 8).Select(p => $"Contact_{p}"));
+    }
+}

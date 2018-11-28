47766d771478a3857f3e31c5c1669b4825a0d3fc
Miňo Martiniak <mino.martiniak@gmail.com>
2018-11-28 21:09:17 +0100

    Add endpoints into combo

---
+++ src/MMLib.SwaggerForOcelot/BuilderExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+
+namespace MMLib.SwaggerForOcelot
+{
+    public static class BuilderExtensions
+    {
+        public static IApplicationBuilder UseSwaggerForOcelot(this IApplicationBuilder app, IConfiguration configuration)
+        {
+            app.UseSwaggerUI(c =>
+            {
+                foreach (var reRoute in configuration.GetSection("ReRoutes").Get<IEnumerable<ReRouteOption>>())
+                {
+                    c.SwaggerEndpoint(reRoute.SwaggerEndPoint.Url, reRoute.SwaggerEndPoint.Name);
+                }
+            });
+
+            return app;
+        }
+    }
+}
---
+++ src/MMLib.SwaggerForOcelot/ReRouteOption.cs
@@ -0,0 +1,14 @@
+﻿namespace MMLib.SwaggerForOcelot
+{
+    internal class ReRouteOption
+    {
+        public SwaggerEndPointOption SwaggerEndPoint { get; set; }
+    }
+
+    internal class SwaggerEndPointOption
+    {
+        public string Name { get; set; }
+
+        public string Url { get; set; }
+    }
+}

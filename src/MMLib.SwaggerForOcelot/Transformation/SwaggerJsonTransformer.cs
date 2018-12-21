53ba2b3fab2d5f759e742f88f7f1bc66ca33441a
Felipe Lambert <felipe.lambert@globalweb.com.br>
2018-12-21 12:20:36 -0200

    Add JObject to make changes apropriate on swagger

--- src/MMLib.SwaggerForOcelot/Transformation/SwaggerJsonTransformer.cs
+++ src/MMLib.SwaggerForOcelot/Transformation/SwaggerJsonTransformer.cs
@@ -1,4 +1,5 @@
 ﻿using MMLib.SwaggerForOcelot.Configuration;
+using Newtonsoft.Json.Linq;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,12 +16,61 @@ namespace MMLib.SwaggerForOcelot.Transformation
         /// <inheritdoc/>
         public string Transform(string swaggerJson, IEnumerable<ReRouteOptions> reRoutes)
         {
-            var sb = new StringBuilder(swaggerJson);
-            var route = reRoutes.First();            
+            var route = reRoutes.First();
 
-            sb.Replace(route.DownstreamPath, route.UpstreamPath);
+            JObject swagger = JObject.Parse(swaggerJson);
+            swagger.Remove("host");
+            if (swagger["paths"] != null)
+            {
+                for (int i = 0; i < swagger["paths"].Count(); i++)
+                {
 
-            return sb.ToString();
+                    string down = ((JProperty)swagger["paths"].ElementAt(i)).Name;
+
+                    RenameToken(swagger["paths"].ElementAt(i), ReplaceFirst(down, route.DownstreamPath, route.UpstreamPath));
+                }
+            }
+
+            //sb.Replace(route.DownstreamPath, route.UpstreamPath);
+
+            return swagger.ToString(Newtonsoft.Json.Formatting.Indented);
         }
+
+        private string ReplaceFirst(string text, string search, string replace)
+        {
+            int pos = text.IndexOf(search);
+            if (pos < 0)
+            {
+                return text;
+            }
+            return text.Substring(0, pos) + replace + text.Substring(pos + search.Length);
+        }
+
+        public static void RenameToken(JToken token, string newName)
+        {
+            //if (token == null)
+            //    throw new ArgumentNullException("token", "Cannot rename a null token");
+
+            JProperty property;
+
+            if (token.Type == JTokenType.Property)
+            {
+                //if (token.Parent == null)
+                //    throw new InvalidOperationException("Cannot rename a property with no parent");
+
+                property = (JProperty)token;
+            }
+            else
+            {
+                //if (token.Parent == null || token.Parent.Type != JTokenType.Property)
+                //    throw new InvalidOperationException("This token's parent is not a JProperty; cannot rename");
+
+                property = (JProperty)token.Parent;
+            }
+
+            var newProperty = new JProperty(newName, property.Value);
+            property.Replace(newProperty);
+        }
+
     }
 }

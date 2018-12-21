ac2626a3081f59e4c1e0acf84cb09d12e49bf363
Felipe Lambert <felipe.lambert@globalweb.com.br>
2018-12-21 12:24:29 -0200

    Update Rename to swagger match Pattern

--- src/MMLib.SwaggerForOcelot/Transformation/SwaggerJsonTransformer.cs
+++ src/MMLib.SwaggerForOcelot/Transformation/SwaggerJsonTransformer.cs
@@ -48,25 +48,9 @@ namespace MMLib.SwaggerForOcelot.Transformation
 
         public static void RenameToken(JToken token, string newName)
         {
-            //if (token == null)
-            //    throw new ArgumentNullException("token", "Cannot rename a null token");
-
             JProperty property;
 
-            if (token.Type == JTokenType.Property)
-            {
-                //if (token.Parent == null)
-                //    throw new InvalidOperationException("Cannot rename a property with no parent");
-
-                property = (JProperty)token;
-            }
-            else
-            {
-                //if (token.Parent == null || token.Parent.Type != JTokenType.Property)
-                //    throw new InvalidOperationException("This token's parent is not a JProperty; cannot rename");
-
-                property = (JProperty)token.Parent;
-            }
+            property = (JProperty)token;
 
             var newProperty = new JProperty(newName, property.Value);
             property.Replace(newProperty);

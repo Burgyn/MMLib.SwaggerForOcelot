34dd25b5c3498553ae425d49249b22a525ffc6f8
Miňo Martiniak <mino.martiniak@gmail.com>
2020-01-07 19:47:19 +0100

    Refactor/56 new unit testing (#63)
    * New way of unit testing swagger transformation.
    
    * Replace old tests with new type.
    
    * Rewrite open api tests to new format
    
    * Docs fixed

---
+++ tests/MMLib.SwaggerForOcelot.Tests/SwaggerForOcelotShould.cs
@@ -0,0 +1,45 @@
+using System;
+using FluentAssertions;
+using JsonDiffPatchDotNet;
+using MMLib.SwaggerForOcelot.Transformation;
+using Newtonsoft.Json.Linq;
+using Xunit;
+using Xunit.Sdk;
+
+namespace MMLib.SwaggerForOcelot.Tests
+{
+    /// <summary>
+    /// Generic tests for transformation downstream swagger to upstream format. 
+    /// Source test cases are located as resources in '/tests' folder and providing by <see cref="TestCasesProvider" />.
+    /// </summary>
+    public class SwaggerForOcelotShould
+    {
+        [Theory]
+        [ClassData(typeof(TestCasesProvider))]
+        public void TransferDownstreamSwaggerToUpstreamFormat(TestCase testData)
+        {
+            var transformer = new SwaggerJsonTransformer();
+
+            var transformed = transformer.Transform(
+                testData.DownstreamSwagger.ToString(),
+                testData.ReRoutes,
+                testData.HostOverride);
+
+            AreEqual(transformed, testData.ExpectedTransformedSwagger);
+        }
+
+        private static void AreEqual(string transformed, JObject expected)
+        {
+            JObject actual = JObject.Parse(transformed);
+
+            if (!JObject.DeepEquals(actual, expected))
+            {
+                var jdp = new JsonDiffPatch();
+                JToken patch = jdp.Diff(actual, expected);
+
+                throw new XunitException(
+                    $"Transformed upstream swagger is not equal to expected. {Environment.NewLine} Diff: {patch.ToString()}");
+            }
+        }
+    }
+}

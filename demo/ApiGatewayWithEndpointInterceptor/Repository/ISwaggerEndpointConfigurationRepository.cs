16480ea21b51c2e99edced6b3093a5e20aac1427
Hantsch <darkpageat@gmail.com>
2020-12-18 07:10:38 +0100

    Added a downstream interceptor interface which can be injected optionally to control downstream of an endpoint.

---
+++ demo/ApiGatewayWithEndpointInterceptor/Repository/ISwaggerEndpointConfigurationRepository.cs
@@ -0,0 +1,9 @@
+﻿using MMLib.SwaggerForOcelot.Configuration;
+
+namespace ApiGatewayWithEndpointSecurity.Repository
+{
+    public interface ISwaggerEndpointConfigurationRepository
+    {
+        ManageSwaggerEndpointData GetSwaggerEndpoint(SwaggerEndPointOptions endPoint, string version);
+    }
+}

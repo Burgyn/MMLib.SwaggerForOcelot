1b823ae6477a2767c5c35a6c2cdaded46ce4e95f
Hantsch <darkpageat@gmail.com>
2020-12-18 08:17:10 +0100

    added documentation section in readme about downstream intercceptor interface

--- demo/ApiGatewayWithEndpointInterceptor/Interceptor/PublishedDownstreamInterceptor.cs
+++ demo/ApiGatewayWithEndpointInterceptor/Interceptor/PublishedDownstreamInterceptor.cs
@@ -5,26 +5,26 @@ using MMLib.SwaggerForOcelot.Middleware;
 
 namespace ApiGatewayWithEndpointSecurity.Interceptor
 {
-    public class PublishedDownstreamInterceptor : ISwaggerDownstreamInterceptor
+public class PublishedDownstreamInterceptor : ISwaggerDownstreamInterceptor
+{
+    private readonly ISwaggerEndpointConfigurationRepository _endpointConfigurationRepository;
+
+    public PublishedDownstreamInterceptor(ISwaggerEndpointConfigurationRepository endpointConfigurationRepository)
     {
-        private readonly ISwaggerEndpointConfigurationRepository _endpointConfigurationRepository;
-
-        public PublishedDownstreamInterceptor(ISwaggerEndpointConfigurationRepository endpointConfigurationRepository)
-        {
-            _endpointConfigurationRepository = endpointConfigurationRepository;
-        }
-
-        public bool DoDownstreamSwaggerEndpoint(HttpContext httpContext, string version, SwaggerEndPointOptions endPoint)
-        {
-            var myEndpointConfiguration = this._endpointConfigurationRepository.GetSwaggerEndpoint(endPoint, version);
-
-            if (!myEndpointConfiguration.IsPublished)
-            {
-                httpContext.Response.StatusCode = 404;
-                httpContext.Response.WriteAsync("This enpoint is under development, please come back later.");
-            }
-
-            return myEndpointConfiguration.IsPublished;
-        }
+        _endpointConfigurationRepository = endpointConfigurationRepository;
     }
+
+    public bool DoDownstreamSwaggerEndpoint(HttpContext httpContext, string version, SwaggerEndPointOptions endPoint)
+    {
+        var myEndpointConfiguration = _endpointConfigurationRepository.GetSwaggerEndpoint(endPoint, version);
+
+        if (!myEndpointConfiguration.IsPublished)
+        {
+            httpContext.Response.StatusCode = 404;
+            httpContext.Response.WriteAsync("This enpoint is under development, please come back later.");
+        }
+
+        return myEndpointConfiguration.IsPublished;
+    }
+}
 }

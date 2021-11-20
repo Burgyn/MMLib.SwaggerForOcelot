72cdab05523e554cc727db03529f2c53d4dd78d4
Kevin Bodnár <bodnar.kevin@gmail.com>
2021-11-20 15:11:48 +0100

    Add AuthenticationOptions missing comments

--- src/MMLib.SwaggerForOcelot/Configuration/AuthenticationOptions.cs
+++ src/MMLib.SwaggerForOcelot/Configuration/AuthenticationOptions.cs
@@ -2,10 +2,21 @@
 
 namespace MMLib.SwaggerForOcelot.Configuration
 {
+    /// <summary>
+    /// Ocelot AuthenticationOptions configuration.
+    /// </summary>
     public class AuthenticationOptions
     {
+        /// <summary>
+        /// Ocelot AuthenticationProviderKey configuration.
+        /// Authentication provider key.
+        /// </summary>
         public string AuthenticationProviderKey { get; set; }
 
+        /// <summary>
+        /// Ocelot AllowedScopes configartion.
+        /// Ocelot will get all scope claims and make sure that the user has all of them.
+        /// </summary>
         public List<string> AllowedScopes { get; set; }
     }
 }

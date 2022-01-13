57bd9412254747bb659fe2cd2557068a7e4669f3
Miňo Martiniak <mino.martiniak@gmail.com>
2022-01-13 21:41:53 +0100

    benchmark test (#216)
    * benchmark test
    
    * changes
    
    * Upgrade test framework
    
    * run benchmark
    
    * remove codeql analyze

--- tests/MMLib.SwaggerForOcelot.BenchmarkTests/Program.cs
+++ tests/MMLib.SwaggerForOcelot.BenchmarkTests/Program.cs
@@ -1,13 +1,4 @@
-﻿using System;
-using BenchmarkDotNet.Running;
+﻿using BenchmarkDotNet.Running;
+using MMLib.SwaggerForOcelot.BenchmarkTests;
 
-namespace MMLib.SwaggerForOcelot.BenchmarkTests
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var summary = BenchmarkRunner.Run<SwaggerJsonTransfromerBenchmark>();
-        }
-    }
-}
+BenchmarkRunner.Run<SwaggerJsonTransfromerBenchmark>();
--- tests/MMLib.SwaggerForOcelot.BenchmarkTests/SwaggerJsonTransfromerBenchmark.cs
+++ tests/MMLib.SwaggerForOcelot.BenchmarkTests/SwaggerJsonTransfromerBenchmark.cs
@@ -1,71 +1,62 @@
-using System;
 using BenchmarkDotNet.Attributes;
 using MMLib.SwaggerForOcelot.Configuration;
 using MMLib.SwaggerForOcelot.Transformation;
-using System.Collections;
-using System.Linq;
 using System.Collections.Generic;
 using System.IO;
-using Newtonsoft.Json;
 using System.Reflection;
 using System.Text;
 
-namespace MMLib.SwaggerForOcelot.BenchmarkTests
+namespace MMLib.SwaggerForOcelot.BenchmarkTests;
+
+public class SwaggerJsonTransfromerBenchmark
 {
-    public class SwaggerJsonTransfromerBenchmark
+    private const string RootNamespaceResources = "MMLib.SwaggerForOcelot.BenchmarkTests.Resources";
+    private readonly string _swagger;
+    private readonly SwaggerJsonTransformer _transformer;
+    private readonly List<RouteOptions> _routeOptions;
+
+    public SwaggerJsonTransfromerBenchmark()
     {
-        private const string RootNamespaceResources = "MMLib.SwaggerForOcelot.BenchmarkTests.Resources";
-        private string _swagger;
-        private SwaggerJsonTransformer _transformer;
-        private List<ReRouteOptions> _reRouteOptions;
+        _swagger = ReadFile("Swagger.json");
 
-        public SwaggerJsonTransfromerBenchmark()
+        _routeOptions = new List<RouteOptions>
         {
-            _swagger = ReadFile("Swagger.json");
-            _reRouteOptions = new List<ReRouteOptions>
+            new()
             {
-                new ReRouteOptions
-                {
-                    SwaggerKey = "pets",
-                    UpstreamPathTemplate ="/v2/api/pets/pet/{everything}",
-                    DownstreamPathTemplate ="/v2/pet/{everything}",
-                },
-                new ReRouteOptions
-                {
-                    SwaggerKey = "pets",
-                    UpstreamPathTemplate = "/v2/api/pets/store/{everything}",
-                    DownstreamPathTemplate = "/v2/store/{everything}",
-                },
-                new ReRouteOptions
-                {
-                    SwaggerKey = "pets",
-                    UpstreamPathTemplate = "/v2/api/pets/user/{everything}",
-                    DownstreamPathTemplate = "/v2/user/{everything}",
-                }
-            };
-
-            _transformer = new SwaggerJsonTransformer();
-        }
-
-        [Benchmark]
-        public void Transform()
-        {
-            string transformed = _transformer.Transform(
-                _swagger,
-                _reRouteOptions,
-                string.Empty);
-        }
-
-        private string ReadFile(string testFilePath)
-        {
-            Stream resourceStream = Assembly
-                .GetExecutingAssembly()
-                .GetManifestResourceStream($"{RootNamespaceResources}.{testFilePath}");
-
-            using (var reader = new StreamReader(resourceStream, Encoding.UTF8))
+                SwaggerKey = "pets",
+                UpstreamPathTemplate ="/v2/api/pets/pet/{everything}",
+                DownstreamPathTemplate ="/v2/pet/{everything}",
+            },
+            new()
             {
-                return reader.ReadToEnd();
+                SwaggerKey = "pets",
+                UpstreamPathTemplate = "/v2/api/pets/store/{everything}",
+                DownstreamPathTemplate = "/v2/store/{everything}",
+            },
+            new()
+            {
+                SwaggerKey = "pets",
+                UpstreamPathTemplate = "/v2/api/pets/user/{everything}",
+                DownstreamPathTemplate = "/v2/user/{everything}",
             }
-        }
+        };
+
+        _transformer = new SwaggerJsonTransformer(new OcelotSwaggerGenOptions());
+    }
+
+    [Benchmark]
+    public void Transform()
+    {
+       _transformer.Transform(_swagger, _routeOptions,  string.Empty, false);
+    }
+
+    private static string ReadFile(string testFilePath)
+    {
+        Stream resourceStream = Assembly
+            .GetExecutingAssembly()
+            .GetManifestResourceStream($"{RootNamespaceResources}.{testFilePath}")!;
+
+        using var reader = new StreamReader(resourceStream, Encoding.UTF8);
+        return reader.ReadToEnd();
     }
 }
